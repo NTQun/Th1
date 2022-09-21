c7f81c3efb591debc1847011f31e351d34ba03aa
NTQun <quanb1910130@student.ctu.edu.vn>
2022-09-21 18:49:49 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const  express= require("express");
-const cors =require("cors");
+const cors = require("cors");
+
+const contactsRouters =require("./app/routes/contact.route");
 const app=express ();
+
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.json({message: "welcome to contact book application."});
-});
+
+app.get("/", (req,res) =>{
+     res.json({message: "welcome to contact book application."})
+ });
+app.use("/api/contacts", contactsRouters);
+
+
 module.exports= app;
