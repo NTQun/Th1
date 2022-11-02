47ffc604d083f365f64602dd6eaf937daab6049b
Trung Quan <quanb1910130@student.ctu.edu.vn>
2022-11-02 14:32:43 +0700

    Hien thi danh sach cac contact lay ve tu server

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -131,4 +131,4 @@ exports.deleteAll=async(_req, res, next)=>{
             )
         );
     }
- };
+};
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -30,51 +30,51 @@ class ContactService{
         return result.value;
     }
 
-//     async find(filter){
-//         const cursor = await this.contact.find(filter);
-//         return await cursor.toArray();
-//     }
+    async find(filter){
+        const cursor = await this.contact.find(filter);
+        return await cursor.toArray();
+    }
 
-//     async findByName(name){
-//         return await this.find({
-//             name: { $regex: new RegExp(name), $options:"i"},
-//         });
-//     }
+    async findByName(name){
+        return await this.find({
+            name: { $regex: new RegExp(name), $options:"i"},
+        });
+    }
 
-//     async findById(id){
-//         return await this.contact.findOne({
-//             _id: ObjectId.isValid(id)? new ObjectId(id): null,
-//         });
-//     }
+    async findById(id){
+        return await this.contact.findOne({
+            _id: ObjectId.isValid(id)? new ObjectId(id): null,
+        });
+    }
 
-//     async update(id, payload){
-//         const filter={
-//             _id: ObjectId.isValid(id)?new ObjectId(id):null,
-//         };
-//         const update=this.extractConactData(payload);
-//         const result=await this.contact.findOneAndUpdate(
-//             filter,
-//             { $set: update},
-//             { returnDocument:"after"}
-//         );
-//         return result.value;
-//     }
+    async update(id, payload){
+        const filter={
+            _id: ObjectId.isValid(id)?new ObjectId(id):null,
+        };
+        const update=this.extractConactData(payload);
+        const result=await this.contact.findOneAndUpdate(
+            filter,
+            { $set: update},
+            { returnDocument:"after"}
+        );
+        return result.value;
+    }
 
-//     async delete(id){
-//         const result = await this.contact.findOneAndDelete({
-//             _id: ObjectId.isValid(id)? new ObjectId(id):null,
-//         });
-//         return result.value;
-//     }
+    async delete(id){
+        const result = await this.contact.findOneAndDelete({
+            _id: ObjectId.isValid(id)? new ObjectId(id):null,
+        });
+        return result.value;
+    }
 
-//     async findFavorite(){
-//         return await this.find({favorite: true});
-//     }
+    async findFavorite(){
+        return await this.find({favorite: true});
+    }
 
-//     async deleteAll(){
-//         const result=await this.contact.deleteMany({});
-//         return result.deletedCount;
-//     }
+    async deleteAll(){
+        const result=await this.contact.deleteMany({});
+        return result.deletedCount;
+    }
 }
 
 module.exports = ContactService;
